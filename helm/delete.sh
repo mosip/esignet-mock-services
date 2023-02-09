513ea0dd42155cf5a7f0e458ca239f1827b47472
Akila Lakshmanan <77330852+akilalakshmanan@users.noreply.github.com>
2023-02-09 14:00:21 +0530

    [DSD-2109] Restructured helm charts for OIDC Server and OIDC UI (#9)
    * [DSD-2109] Restructured helm charts for OIDC Server and OIDC UI
    
    * [DSD-2109] Removed tgz directory
    
    * [DSD-2109] Updated copy_cm script

---
+++ helm/delete.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Uninstalls mock-relying-party-service and mock-relying-party-ui
+## Usage: ./delete.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
+NS=esignet
+while true; do
+    read -p "Are you sure you want to delete esignet mock service helm charts?(Y/n) " yn
+    if [ $yn = "Y" ]
+      then
+        helm -n $NS delete mock-relying-party-service
+        helm -n $NS delete mock-relying-party-ui
+        break
+      else
+        break
+    fi
+done
