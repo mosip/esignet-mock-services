b286369eeca0de86ea1bae0f2840f94bba620785
Rakshithb1 <79500257+Rakshithb1@users.noreply.github.com>
2023-02-27 10:43:34 +0530

    [DSD-2167] added helm charts for mock-identity-system (#22)
    * [DSD-2167] added helm charts for mock-identity-system
    
    * Update install.sh
    
    * Update deployment.yaml

---
+++ helm/mock-identity-system/copy_cm.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Copy configmaps from other namespaces
+# DST_NS: Destination namespace 
+
+COPY_UTIL=../copy_cm_func.sh
+DST_NS=mock-identity-system
+
+$COPY_UTIL configmap global default $DST_NS 
+$COPY_UTIL configmap artifactory-share artifactory $DST_NS 
+$COPY_UTIL configmap config-server-share config-server $DST_NS
+$COPY_UTIL configmap softhsm-mock-identity-system-share softhsm $DST_NS
