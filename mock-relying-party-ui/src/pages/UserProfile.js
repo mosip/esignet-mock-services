dd707fcceab4dfb507babdd13908abb4b7ee2b42
YaswanthNagarjuna <nagarjuna13598@gmail.com>
2023-02-27 17:39:44 +0530

    ProfileUI design Updated

--- mock-relying-party-ui/src/pages/UserProfile.js
+++ mock-relying-party-ui/src/pages/UserProfile.js
@@ -2,13 +2,14 @@ import React from "react";
 import Background from "../components/Background";
 import UserProfile from "../components/UserProfile";
 import relyingPartyService from "../services/relyingPartyService";
+import ProfileUI from "../components/ProfileUI";
+
 
 export default function UserProfilePage({ langOptions }) {
   return (
-    <Background
-      component={React.createElement(UserProfile, {
-        relyingPartyService: relyingPartyService,
-      })}
+    <ProfileUI
+        relyingPartyService= {relyingPartyService}
+      
       langOptions={langOptions}
     />
   );
