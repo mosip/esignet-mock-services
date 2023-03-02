84f4e3e934039076c063a6275cd60e57a3293611
YaswanthNagarjuna <nagarjuna13598@gmail.com>
2023-03-02 17:38:38 +0530

    Sidenav bar updated to common component

--- mock-relying-party-ui/src/pages/UserProfile.js
+++ mock-relying-party-ui/src/pages/UserProfile.js
@@ -1,11 +1,12 @@
 import React from "react";
 import relyingPartyService from "../services/relyingPartyService";
 import ProfileUI from "../components/ProfileUI";
-
+import Sidenav from "../components/Sidenav";
 
 export default function UserProfilePage({ langOptions }) {
   return (
-    <ProfileUI
-      relyingPartyService={relyingPartyService} />
+    <Sidenav relyingPartyService={relyingPartyService}  component={React.createElement(ProfileUI, {
+      relyingPartyService:relyingPartyService
+    })}/>
   );
 }
