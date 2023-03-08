61d6212667f71e5a07d11d0f6ab81f5028501d2d
Himaja Dhanyamraju <43470317+HimajaDhanyamraju2@users.noreply.github.com>
2023-03-08 20:51:12 +0530

    Updated ddl.sql (#38)
    * MOSIP-25755: Added mock-esignet-integration-impl module
    
    * MOSIP-25755: pom changes
    
    * Removed docker file
    
    * Modified properties and dependencies
    
    * Added new param to doKeyBinding
    
    * Removed duplicates
    
    * MOSIP-25755: Changes in pom file
    
    * Rolledback db configs
    
    * Modified module name under parent pom
    
    * Modified properties files
    
    * MOSIP-26295: Modified LoggerAuditService
    
    * Updated ddl.sql
    
    ---------
    
    Co-authored-by: M1044292 <M1044292@C1ML54597.mindtree.com>

--- db_scripts/mosip_mockidentitysystem/ddl.sql
+++ db_scripts/mosip_mockidentitysystem/ddl.sql
@@ -3,6 +3,6 @@
 \ir ddl/mockidentitysystem-mock_identity.sql
 \ir ddl/mockidentitysystem-kyc_auth.sql
 \ir ddl/mockidentitysystem-key_alias.sql
-\ir ddl/mockidentitysystem-kyc_policy_def.sql
-\ir ddl/mockidentitysystem-kyc_store.sql
+\ir ddl/mockidentitysystem-key_policy_def.sql
+\ir ddl/mockidentitysystem-key_store.sql
 
