13ca0acb08ef0b55b926fe0d3f03c2bd6fae62cb
akilalakshmanan <Akila.L@iiitb.ac.in>
2023-03-20 15:21:56 +0530

    [DSD-2300] Added softhsm configmap for mock-identity-system

--- helm/copy_cm.sh
+++ helm/copy_cm.sh
@@ -9,3 +9,4 @@ DST_NS=esignet
 $COPY_UTIL configmap global default $DST_NS
 $COPY_UTIL configmap config-server-share config-server $DST_NS
 $COPY_UTIL configmap artifactory-share artifactory $DST_NS
+$COPY_UTIL configmap softhsm-mock-identity-system-share softhsm $DST_NS
