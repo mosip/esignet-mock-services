73317e4195e1d3527ed7bc127c8440a0c8e915f9
syed salman <72004356+syedsalman3753@users.noreply.github.com>
2023-04-12 12:52:13 +0530

    [ MOSIP-26666 ] updated README.md & deployment scripts (#61)
    * [ MOSIP-26666 ] updated README.md
    
    * [ MOSIP-26666 ] updated README.md
    
    * [ MOSIP-26666 ] Updated esignet-mock README.md and deployment scripts
    
    * [MOSIP-26666]
    
    * [ MOSIP-26666 ] Updated README.md
    
    * [ MOSIP-26666 ] updated mock-relying-party-service install.sh
    
    * [MOSIP-26666]
    
    * [ MOSIP-26666 ] set default value for ESIGNET_SERVICE_URL
    
    * [ MOSIP-26666 ] updated chart url in release_chart.yml
    
    ---------
    
    Co-authored-by: syed-salman-technoforte <syed.salman@technoforte.co.in>
    Co-authored-by: Keshav Mishra <chandrakeshavmishra@gmail.com>

--- helm/mock-identity-system/copy_cm.sh
+++ helm/mock-identity-system/copy_cm.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Copy configmaps from other namespaces
-# DST_NS: Destination namespace 
+# DST_NS: Destination namespace
 
 COPY_UTIL=../copy_cm_func.sh
-DST_NS=mock-identity-system
+DST_NS=esignet
 
-$COPY_UTIL configmap global default $DST_NS 
-$COPY_UTIL configmap artifactory-share artifactory $DST_NS 
+$COPY_UTIL configmap global default $DST_NS
+$COPY_UTIL configmap artifactory-share artifactory $DST_NS
 $COPY_UTIL configmap config-server-share config-server $DST_NS
 $COPY_UTIL configmap softhsm-mock-identity-system-share softhsm $DST_NS
