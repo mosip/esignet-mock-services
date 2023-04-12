9231f405dfeae19e44e8a56b7affa0d3d5b627cc
syed salman <72004356+syedsalman3753@users.noreply.github.com>
2023-04-12 12:52:13 +0530

    [ MOSIP-26666 ] updated README.md & deployment scripts (#61)
    * [ MOSIP-26666 ] updated README.md
    
    * [ MOSIP-26666 ] updated README.md
    
    * [ MOSIP-26666 ] Updated esignet-mock README.md and deployment scripts
    
    * [MOSIP-26666]
    
    * [ MOSIP-26666 ] Updated README.md
    
    * [ MOSIP-26666 ] updated mock-relying-party-service install.sh
    
    * [MOSIP-26666]
    
    * [ MOSIP-26666 ] set default value for ESIGNET_SERVICE_URL
    
    * [ MOSIP-26666 ] updated chart url in release_chart.yml
    
    ---------
    
    Co-authored-by: syed-salman-technoforte <syed.salman@technoforte.co.in>
    Co-authored-by: Keshav Mishra <chandrakeshavmishra@gmail.com>
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

---
+++ helm/mock-relying-party-ui/install.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+# Installs all esignet helm charts
+## Usage: ./install.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
+NS=esignet
+CHART_VERSION=0.9.0
+
+read -p "Please provide mock relying party ui domain (eg: healthservices.sandbox.xyz.net ) : " MOCK_UI_HOST
+
+if [ -z "$MOCK_UI_HOST" ]; then
+  echo "Mock relying party UI Host not provided; EXITING;"
+  exit 0;
+fi
+
+CHK_MOCK_UI_HOST=$( nslookup "$MOCK_UI_HOST" )
+if [ $? -gt 0 ]; then
+  echo "Mock relying party UI Host does not exists; EXITING;"
+  exit 0;
+fi
+
+echo Create $NS namespace
+kubectl create ns $NS
+
+echo Istio label
+kubectl label ns $NS istio-injection=enabled --overwrite
+
+ESIGNET_HOST=$(kubectl get cm global -o jsonpath={.data.mosip-esignet-host})
+
+echo Installing Mock Relying Party UI
+helm -n $NS install mock-relying-party-ui mosip/mock-relying-party-ui \
+    --set mock_relying_party_ui.mock_relying_party_ui_service_host="$MOCK_UI_HOST" \
+    --set mock_relying_party_ui.ESIGNET_UI_BASE_URL="https://$ESIGNET_HOST" \
+    --set mock_relying_party_ui.MOCK_RELYING_PARTY_SERVER_URL="https://$MOCK_UI_HOST/mock-relying-party-service" \
+    --set mock_relying_party_ui.REDIRECT_URI="https://$MOCK_UI_HOST/userprofile" \
+    --set mock_relying_party_ui.REDIRECT_URI_REGISTRATION="https://$MOCK_UI_HOST/registration" \
+    --set istio.hosts\[0\]="$MOCK_UI_HOST" \
+    --version $CHART_VERSION
+
+kubectl -n $NS get deploy mock-relying-party-ui -o name |  xargs -n1 -t  kubectl -n $NS rollout status
+
+echo Installed mock-relying-party-service service
