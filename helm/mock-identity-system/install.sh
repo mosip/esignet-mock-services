4836374ec8aad1915a29456d3497258191f07b99
syed-salman-technoforte <syed.salman@technoforte.co.in>
2023-05-30 15:49:31 +0530

    [MOSIP-27593] Updated charts, install.sh, and README.md

--- helm/mock-identity-system/install.sh
+++ helm/mock-identity-system/install.sh
@@ -18,8 +18,22 @@ echo "Copy configmaps"
 echo Istio label
 kubectl label ns $NS istio-injection=enabled --overwrite
 
+echo "Do you have public domain & valid SSL? (Y/n) "
+echo "Y: if you have public domain & valid ssl certificate"
+echo "n: If you don't have a public domain and a valid SSL certificate. Note: It is recommended to use this option only in development environments."
+read -p "" flag
+
+if [ -z "$flag" ]; then
+  echo "'flag' was provided; EXITING;"
+  exit 1;
+fi
+ENABLE_INSECURE=''
+if [ "$flag" = "n" ]; then
+  ENABLE_INSECURE='--set enable_insecure=true';
+fi
+
 echo Installing mock-identity-system
-helm -n $NS install mock-identity-system mosip/mock-identity-system --version $CHART_VERSION
+helm -n $NS install mock-identity-system mosip/mock-identity-system --version $CHART_VERSION $ENABLE_INSECURE
 
 kubectl -n $NS get deploy mock-identity-system -o name |  xargs -n1 -t  kubectl -n $NS rollout status
 
