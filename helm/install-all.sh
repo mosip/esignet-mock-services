b856dc9645972b6ad725af3839821d4ef80b5a5d
Rakshithb1 <79500257+Rakshithb1@users.noreply.github.com>
2023-07-11 13:17:52 +0530

    [MOSIP-27549] added error handling (#98)
    * [MOSIP-27549] added error handling
    
    * [MOSIP-27549] updated copy_cm.sh

--- helm/install-all.sh
+++ helm/install-all.sh
@@ -10,32 +10,43 @@ ROOT_DIR=`pwd`
 NS=softhsm
 SOFTHSM_CHART_VERSION=12.0.1-B2
 
-echo Istio label
-kubectl label ns $SOFTHSM_NS istio-injection=enabled --overwrite
-helm repo add mosip https://mosip.github.io/mosip-helm
-helm repo update
+function installing_All()
+  echo Istio label
+  kubectl label ns $SOFTHSM_NS istio-injection=enabled --overwrite
+  helm repo add mosip https://mosip.github.io/mosip-helm
+  helm repo update
 
-echo Installing Softhsm for mock-identity-system
-helm -n $NS install softhsm-mock-identity-system mosip/softhsm -f softhsm-values.yaml --version $SOFTHSM_CHART_VERSION --wait
-echo Installed Softhsm for mock-identity-system
+  echo Installing Softhsm for mock-identity-system
+  helm -n $NS install softhsm-mock-identity-system mosip/softhsm -f softhsm-values.yaml --version $SOFTHSM_CHART_VERSION --wait
+  echo Installed Softhsm for mock-identity-system
 
-./copy_cm_func.sh secret softhsm-mock-identity-system softhsm config-server
+  ./copy_cm_func.sh secret softhsm-mock-identity-system softhsm config-server
 
-kubectl -n config-server set env --keys=security-pin --from secret/softhsm-mock-identity-system deployment/config-server --prefix=SPRING_CLOUD_CONFIG_SERVER_OVERRIDES_SOFTHSM_MOCK_IDENTITY_SYSTEM_
-kubectl -n config-server rollout restart deploy config-server
-kubectl -n config-server rollout status config-server
+  kubectl -n config-server set env --keys=security-pin --from secret/softhsm-mock-identity-system deployment/config-server --prefix=SPRING_CLOUD_CONFIG_SERVER_OVERRIDES_SOFTHSM_MOCK_IDENTITY_SYSTEM_
+  kubectl -n config-server rollout restart deploy config-server
+  kubectl -n config-server rollout status config-server
 
-declare -a module=("mock-identity-system"
-                   "mock-relying-party-service"
-		           "mock-relying-party-ui"
-                  )
+  declare -a module=("mock-identity-system"
+                     "mock-relying-party-service"
+                 "mock-relying-party-ui"
+                    )
 
-echo Installing esignet mock services
+  echo Installing esignet mock services
 
-for i in "${module[@]}"
-do
-  cd $ROOT_DIR/"$i"
-  ./install.sh
-done
+  for i in "${module[@]}"
+  do
+    cd $ROOT_DIR/"$i"
+    ./install.sh
+  done
 
-echo All esignet mock services deployed sucessfully.
+  echo All esignet mock services deployed sucessfully.
+  return 0
+}
+
+# set commands for error handling.
+set -e
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
+installing_All   # calling function
