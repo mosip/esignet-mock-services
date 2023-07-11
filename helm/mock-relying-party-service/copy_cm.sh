04f51eb9903d16e7c22bd84f3d2a2b202f5bffd2
Rakshithb1 <rakshit.b@technoforte.co.in>
2023-07-11 20:57:40 +0530

    [MOSIP-27549] updated scripts

--- helm/mock-relying-party-service/copy_cm.sh
+++ helm/mock-relying-party-service/copy_cm.sh
@@ -4,7 +4,7 @@
 
 function copying_cm() {
   COPY_UTIL=../copy_cm_func.sh
-  DST_NS=apolo
+  DST_NS=esignet
 
   $COPY_UTIL configmap global default $DST_NS
   $COPY_UTIL configmap config-server-share config-server $DST_NS
