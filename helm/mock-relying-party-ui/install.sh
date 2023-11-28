5b5a2300f973ddcaf1e7665df4e6784bdd8ba39c
syed salman <72004356+syedsalman3753@users.noreply.github.com>
2023-11-28 17:59:49 +0530

    [DSD-3884] updated deployment & charts as per esignet vc release (#169)
    * [DSD-3884] updated deployment & charts as per esignet vc release
    
    Signed-off-by: syed salman <syedsalman3753@gmail.com>
    
    * [DSD-3884] updated chart version
    
    Signed-off-by: syed salman <syedsalman3753@gmail.com>
    
    ---------
    
    Signed-off-by: syed salman <syedsalman3753@gmail.com>

--- helm/mock-relying-party-ui/install.sh
+++ helm/mock-relying-party-ui/install.sh
@@ -7,7 +7,7 @@ if [ $# -ge 1 ] ; then
 fi
 
 NS=esignet
-CHART_VERSION=0.9.0
+CHART_VERSION=0.9.1
 
 read -p "Please provide mock relying party ui domain (eg: healthservices.sandbox.xyz.net ) : " MOCK_UI_HOST
 
