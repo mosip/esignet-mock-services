9361ed5cc8bfe1f9134a6c7806e251e6d0598e1b
Zeeshan Mehboob <zmahboob00@gmail.com>
2024-01-22 13:50:48 +0530

    [REVERT] reverting ui_locales checking
    Signed-off-by: Zeeshan Mehboob <zmahboob00@gmail.com>

--- mock-relying-party-ui/src/i18n.js
+++ mock-relying-party-ui/src/i18n.js
@@ -3,17 +3,6 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 
-
-// Function to extract query parameters from the URL
-const getQueryParam = (param) => {
-    // getting all query params
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    return urlSearchParams.get(param);
-}
-
-// Extract ui_locales and its value, from the current url
-const uiLocales = getQueryParam('ui_locales');
-
 i18n
   // detect available locale files
   .use(Backend)
@@ -23,7 +12,7 @@ i18n
   .use(initReactI18next)
   // init i18next
   .init({
-    lng: uiLocales ?? window._env_.DEFAULT_LANG,
+    lng: window._env_.DEFAULT_LANG,
     debug: false,
     fallbackLng: "en", //window["envConfigs"].defaultLang, //default language
     interpolation: {
