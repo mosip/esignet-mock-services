341b90e2528ada532f4dd755edc09461f9d9ffa1
Rakshitha650 <76676196+Rakshitha650@users.noreply.github.com>
2024-02-06 19:06:52 +0530

    [DSD-4397]Updated install.sh (#194)
    Signed-off-by: Rakshitha650 <76676196+Rakshitha650@users.noreply.github.com>

--- helm/mock-relying-party-ui/install.sh
+++ helm/mock-relying-party-ui/install.sh
@@ -39,6 +39,7 @@ function installing_mock-relying-party-ui() {
       --set mock_relying_party_ui.REDIRECT_URI="https://$MOCK_UI_HOST/userprofile" \
       --set mock_relying_party_ui.REDIRECT_URI_REGISTRATION="https://$MOCK_UI_HOST/registration" \
       --set mock_relying_party_ui.SIGN_IN_BUTTON_PLUGIN_URL="https://$ESIGNET_HOST/plugins/sign-in-button-plugin.js" \
+      -f values.yaml \
       --set istio.hosts\[0\]="$MOCK_UI_HOST" \
       -f values.yaml \
       --version $CHART_VERSION
