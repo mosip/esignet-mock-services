499db6b0b25dbdc786ab7fece822b68a930ed18a
Kaif Siddique <74772315+kaifk468@users.noreply.github.com>
2024-05-01 16:33:56 +0530

    [ES-940] added support for kba (#209)
    * added support for kba
    
    Signed-off-by: Mohd Kaif Siddique <mohdkaif.siddique@ad.infosys.com>
    
    * reverted local properties to its default values
    
    Signed-off-by: Mohd Kaif Siddique <mohdkaif.siddique@ad.infosys.com>
    
    * make the KBA validation configuration based instead of hardcode
    
    Signed-off-by: Mohd Kaif Siddique <mohdkaif.siddique@ad.infosys.com>
    
    * added lang base validation
    
    Signed-off-by: Mohd Kaif Siddique <mohdkaif.siddique@ad.infosys.com>
    
    ---------
    
    Signed-off-by: Mohd Kaif Siddique <mohdkaif.siddique@ad.infosys.com>
    Co-authored-by: Mohd Kaif Siddique <mohdkaif.siddique@ad.infosys.com>

---
+++ mock-identity-system/src/test/java/io/mosip/esignet/mock/identitysystem/service/impl/AuthenticationServiceImplTest.java
@@ -0,0 +1,121 @@
+package io.mosip.esignet.mock.identitysystem.service.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.mosip.esignet.mock.identitysystem.dto.IdentityData;
+import io.mosip.esignet.mock.identitysystem.dto.KycAuthRequestDto;
+import io.mosip.esignet.mock.identitysystem.dto.KycAuthResponseDto;
+import io.mosip.esignet.mock.identitysystem.dto.LanguageValue;
+import io.mosip.esignet.mock.identitysystem.entity.KycAuth;
+import io.mosip.esignet.mock.identitysystem.exception.MockIdentityException;
+import io.mosip.esignet.mock.identitysystem.repository.AuthRepository;
+import io.mosip.esignet.mock.identitysystem.service.IdentityService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.List;
+import java.util.Map;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AuthenticationServiceImplTest {
+
+    @Mock
+    IdentityService identityService;
+
+    @Mock
+    AuthRepository authRepository;
+
+    @InjectMocks
+    AuthenticationServiceImpl authenticationService;
+
+    @Test
+    public void kycAuth_withValidKbaChallenge_thenPass() {
+
+        List<Map<String,String>> fieldDetailList = List.of(Map.of("id","individualId","type","text","format","string")
+                ,Map.of("id","fullName","type","text","format","")
+                ,Map.of("id","dateOfBirth","type","date","format","yyyy-MM-dd"));
+        ReflectionTestUtils.setField(authenticationService, "fieldDetailList", fieldDetailList);
+        ReflectionTestUtils.setField(authenticationService, "fieldLang", "eng");
+        ReflectionTestUtils.setField(authenticationService,"objectMapper",new ObjectMapper());
+
+        KycAuthRequestDto kycAuthRequestDto = new KycAuthRequestDto();
+        kycAuthRequestDto.setKba("eyJmdWxsTmFtZSI6IlNpZGRoYXJ0aCBLIE1hbnNvdXIiLCJkYXRlT2ZCaXJ0aCI6IjE5ODctMTEtMjUifQ==");
+        kycAuthRequestDto.setIndividualId("individualId");
+        kycAuthRequestDto.setTransactionId("transactionId");
+
+        IdentityData identityData = new IdentityData();
+        identityData.setDateOfBirth("1987/11/25");
+        LanguageValue languageValue = new LanguageValue();
+        languageValue.setLanguage("eng");
+        languageValue.setValue("Siddharth K Mansour");
+        identityData.setFullName(List.of(languageValue));
+        Mockito.when(identityService.getIdentity(Mockito.anyString())).thenReturn(identityData);
+
+        Mockito.when(authRepository.save(Mockito.any())).thenReturn(new KycAuth());
+
+        KycAuthResponseDto kycAuthResponseDto = authenticationService.kycAuth("relyingPartyId", "clientId", kycAuthRequestDto);
+        Assert.assertTrue(kycAuthResponseDto.isAuthStatus());
+    }
+
+    @Test
+    public void kycAuth_withInCorrectKbaChallenge_thenFail() {
+
+        List<Map<String,String>> fieldDetailList = List.of(Map.of("id","individualId","type","text","format","")
+                ,Map.of("id","fullName","type","text","format","")
+                ,Map.of("id","dateOfBirth","type","date","format","yyyy-MM-dd"));
+        ReflectionTestUtils.setField(authenticationService, "fieldDetailList", fieldDetailList);
+        ReflectionTestUtils.setField(authenticationService, "fieldLang", "eng");
+        ReflectionTestUtils.setField(authenticationService,"objectMapper",new ObjectMapper());
+        KycAuthRequestDto kycAuthRequestDto = new KycAuthRequestDto();
+        kycAuthRequestDto.setKba("eyJmdWxsTmFtZSI6IlNpZGRoYXJ0aCBLIiwiZG9iIjoiMTk4Ny0xMS0yNSJ9");
+        kycAuthRequestDto.setIndividualId("individualId");
+        kycAuthRequestDto.setTransactionId("transactionId");
+
+        IdentityData identityData = new IdentityData();
+        identityData.setDateOfBirth("1987/11/25");
+        LanguageValue languageValue = new LanguageValue();
+        languageValue.setLanguage("eng");
+        languageValue.setValue("Siddharth K Mansour");
+        identityData.setFullName(List.of(languageValue));
+        Mockito.when(identityService.getIdentity(Mockito.anyString())).thenReturn(identityData);
+
+        Mockito.when(authRepository.save(Mockito.any())).thenReturn(new KycAuth());
+
+        KycAuthResponseDto kycAuthResponseDto = authenticationService.kycAuth("relyingPartyId", "clientId", kycAuthRequestDto);
+        Assert.assertFalse(kycAuthResponseDto.isAuthStatus());
+    }
+
+    @Test
+    public void kycAuth_withInValidKbaChallenge_thenFail() {
+
+        List<Map<String,String>> fieldDetailList = List.of(Map.of("id","individualId","type","text","format","")
+                ,Map.of("id","fullName","type","text","format","")
+                ,Map.of("id","dateOfBirth","type","date","format","yyyy-MM-dd"));
+        ReflectionTestUtils.setField(authenticationService, "fieldDetailList", fieldDetailList);
+        ReflectionTestUtils.setField(authenticationService, "fieldLang", "eng");
+        ReflectionTestUtils.setField(authenticationService,"objectMapper",new ObjectMapper());
+
+        KycAuthRequestDto kycAuthRequestDto = new KycAuthRequestDto();
+        kycAuthRequestDto.setKba("xsTmFtZSI6IlNpZG0aCBLIiwiZG9iIjoiMTk4Ny0xMS0yNSJ9");
+        kycAuthRequestDto.setIndividualId("individualId");
+        kycAuthRequestDto.setTransactionId("transactionId");
+
+        IdentityData identityData = new IdentityData();
+        identityData.setDateOfBirth("1987/11/25");
+        LanguageValue languageValue = new LanguageValue();
+        languageValue.setLanguage("eng");
+        languageValue.setValue("Siddharth K Mansour");
+        identityData.setFullName(List.of(languageValue));
+        Mockito.when(identityService.getIdentity(Mockito.anyString())).thenReturn(identityData);
+        try{
+            authenticationService.kycAuth("relyingPartyId", "clientId", kycAuthRequestDto);
+        }catch (MockIdentityException e){
+            Assert.assertEquals("auth-failed",e.getMessage());
+        }
+    }
+}
