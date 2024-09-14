e8d47fbd5136bbaa141980f2b7299bbb198ce603
ase-101 <sunkadaeanusha@gmail.com>
2024-09-14 17:21:13 +0530

    Deleted external configuration file
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- mock-identity-system/configure_start.sh
+++ mock-identity-system/configure_start.sh
@@ -15,31 +15,37 @@ if [[ -n "$artifactory_url_env" ]]; then
     echo "Downloaded $artifactory_url_env/$zip_path"
 
     FILE_NAME=${zip_path##*/}
+    DIR_NAME=$hsm_local_dir_name
 else
     FILE_NAME="client.zip"
+    DIR_NAME=$hsm_local_dir
 fi
 
-DIR_NAME=$hsm_local_dir_name
 
-has_parent=$(zipinfo -1 "$FILE_NAME" | awk '{split($NF,a,"/");print a[1]}' | sort -u | wc -l)
-if test "$has_parent" -eq 1; then
-  echo "Zip has a parent directory inside"
-  dirname=$(zipinfo -1 "$FILE_NAME" | awk '{split($NF,a,"/");print a[1]}' | sort -u | head -n 1)
-  echo "Unzip directory"
-  unzip $FILE_NAME
-  echo "Renaming directory"
-  mv -v $dirname $DIR_NAME
+
+if [ "$active_profile_env" != "local" ]; then
+  has_parent=$(zipinfo -1 "$FILE_NAME" | awk '{split($NF,a,"/");print a[1]}' | sort -u | wc -l)
+  if test "$has_parent" -eq 1; then
+    echo "Zip has a parent directory inside"
+    dirname=$(zipinfo -1 "$FILE_NAME" | awk '{split($NF,a,"/");print a[1]}' | sort -u | head -n 1)
+    echo "Unzip directory"
+    unzip $FILE_NAME
+    echo "Renaming directory"
+    mv -v $dirname $DIR_NAME
+  else
+    echo "Zip has no parent directory inside"
+    echo "Creating destination directory"
+    mkdir "$DIR_NAME"
+    echo "Unzip to destination directory"
+    unzip -d "$DIR_NAME" $FILE_NAME
+  fi
+
+  echo "Attempting to install"
+  cd ./$DIR_NAME && chmod +x install.sh && sudo ./install.sh
+  echo "Installation complete"
+  cd $work_dir
+
+  exec "$@"
 else
-  echo "Zip has no parent directory inside"
-  echo "Creating destination directory"
-  mkdir "$DIR_NAME"
-  echo "Unzip to destination directory"
-  unzip -d "$DIR_NAME" $FILE_NAME
+  echo "*** HSM Client installation is ignored in local profile ***"
 fi
-
-echo "Attempting to install"
-cd ./$DIR_NAME && chmod +x install.sh && sudo ./install.sh
-echo "Installation complete"
-cd $work_dir
-
-exec "$@"
