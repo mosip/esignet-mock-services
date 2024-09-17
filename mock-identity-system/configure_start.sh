8d41ec357c6b2757489fe2b6b08d9a7ce26bd673
ase-101 <sunkadaeanusha@gmail.com>
2024-09-17 22:13:07 +0530

    ES-1638
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    Signed-off-by: Zeeshan Mehboob <zeeshan.mehboob@infosys.com>

--- mock-identity-system/configure_start.sh
+++ mock-identity-system/configure_start.sh
@@ -3,24 +3,17 @@
 #installs the pkcs11 libraries.
 set -e
 
-# Check if $artifactory_url_env is not empty
-if [[ -n "$artifactory_url_env" ]]; then
-    DEFAULT_ZIP_PATH=artifactory/libs-release-local/hsm/client.zip
-    [ -z "$hsm_zip_file_path" ] && zip_path="$DEFAULT_ZIP_PATH" || zip_path="$hsm_zip_file_path"
-
-    echo "Download the client from $artifactory_url_env"
-    echo "Zip File Path: $zip_path"
-
-    wget -q --show-progress "$artifactory_url_env/$zip_path"
-    echo "Downloaded $artifactory_url_env/$zip_path"
-
-    FILE_NAME=${zip_path##*/}
+# Check if $hsm_client_zip_url_env is not empty
+if [[ -n "$hsm_client_zip_url_env" ]]; then
+    echo "Download the client from $hsm_client_zip_url_env"
+    wget -q --show-progress "$hsm_client_zip_url_env" -O client.zip
+    echo "Downloaded $hsm_client_zip_url_env"
     DIR_NAME=$hsm_local_dir_name
 else
-    FILE_NAME="client.zip"
     DIR_NAME=$hsm_local_dir
 fi
 
+FILE_NAME="client.zip"
 
 
 if [ "$active_profile_env" != "local" ]; then
