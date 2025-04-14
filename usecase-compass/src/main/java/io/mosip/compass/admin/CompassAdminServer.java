33b0cd783d64c7c0b4517f81759182db128d60dc
Piyush7034 <piyushshukla2100@gmail.com>
2025-04-14 15:12:40 +0530

    Added build actions for usecase-compass service
    Signed-off-by: Piyush7034 <piyushshukla2100@gmail.com>

--- usecase-compass/src/main/java/io/mosip/compass/admin/CompassAdminServer.java
+++ usecase-compass/src/main/java/io/mosip/compass/admin/CompassAdminServer.java
@@ -3,11 +3,6 @@ package io.mosip.compass.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- *
- */
-
 @SpringBootApplication
 public class CompassAdminServer
 {
