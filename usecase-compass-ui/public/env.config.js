2d6e264ced65eca7d64259166e25e1d04ffa79ef
Madhuravas reddy <madhu@mosip.io>
2025-04-16 20:51:33 +0530

    Mosip 41056 chnaged valid admin role to string value (#383)
    * chnaged valid admin role to string value
    
    Signed-off-by: Madhuravas reddy <madhu@mosip.io>
    
    * changed email filed label
    
    Signed-off-by: Madhuravas reddy <madhu@mosip.io>
    
    ---------
    
    Signed-off-by: Madhuravas reddy <madhu@mosip.io>

--- usecase-compass-ui/public/env.config.js
+++ usecase-compass-ui/public/env.config.js
@@ -1,7 +1,7 @@
 window._env_ = {
-  KEYCLOAK_URL: "https://iam.dev2.mosip.net/auth/",
-  KEYCLOAK_REALM: "mosip",
-  KEYCLOAK_CLIENT_ID: "compass-public",
+  KEYCLOAK_URL: "http://localhost:9090",
+  KEYCLOAK_REALM: "compass",
+  KEYCLOAK_CLIENT_ID: "compass-config",
   BASE_URL: "http://localhost:8091/v1/admin",
-  VALID_ADMIN_ROLE: ["COMPASS_ADMIN"]
+  VALID_ADMIN_ROLE: "test_admin"
 };
