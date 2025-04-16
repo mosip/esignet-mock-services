bfa4cb71c3ad3e2411dbf790cccf44d67c600b74
Sreenadh S <32409698+sree96@users.noreply.github.com>
2025-04-16 17:32:18 +0530

    [MOSIP-41056] - update broken ui
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>

--- mock-relying-party-ui/src/pages/Login.js
+++ mock-relying-party-ui/src/pages/Login.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { HomeFeatures } from "../components/Home/HomeFeatures";
 import { Header } from "../components/PageTemplate/Header";
+import {Footer} from "../components/PageTemplate/Footer";
 
 export default function LoginPage({ langOptions }) {
   return (
     <div className={"pb-20 flex flex-col gap-y-4 h-[80%]"}>
       <Header showLogout={false} />
       <HomeFeatures />
+      <Footer/>
     </div>
   );
 }
