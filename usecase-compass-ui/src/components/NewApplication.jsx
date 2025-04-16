8380eef159d1842c95c2168c8405473cb43e5eaa
Madhuravas reddy <madhu@mosip.io>
2025-04-17 00:02:08 +0530

    Mosip 41056 (#387)
    * resolved api response issue
    
    Signed-off-by: Madhuravas reddy <madhu@mosip.io>
    
    * removed typeof response.data
    
    Signed-off-by: Madhuravas reddy <madhu@mosip.io>
    
    ---------
    
    Signed-off-by: Madhuravas reddy <madhu@mosip.io>

--- usecase-compass-ui/src/components/NewApplication.jsx
+++ usecase-compass-ui/src/components/NewApplication.jsx
@@ -8,7 +8,7 @@ const NewApplication = () => {
     const [showSuccessMsg, setShowSuccessMsg] = useState(false);
 
     return (
-        <div className={`${showSuccessMsg ? "flex items-center justify-center bg-[length:100%_100%]" : "bg-[length:100%_50%]"} bg-[url('public/assets/background.png')] bg-no-repeat w-[100%] py-8 px-12 flex justify-center`}>
+        <div className={`${showSuccessMsg ? "flex items-center justify-center bg-[length:100%_100%] py-2 px-12" : "bg-[length:100%_50%] py-8 px-12"} bg-[url('public/assets/background.png')] bg-no-repeat w-[100%] flex justify-center`}>
             {showSuccessMsg ? (<SuccessMsg />) : (<div>
                 <div className="flex space-x-3 mb-8 min-[860px]:-ml-15">
                     <Link to="/home"><img src="/assets/icons/back.svg" alt="back" className="cursor-pointer mt-1" /></Link>
