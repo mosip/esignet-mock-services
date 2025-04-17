fc1d81562895cb1ffb8bff1ceca3f722faf0ce2b
Madhuravas reddy <madhu@mosip.io>
2025-04-17 09:19:46 +0530

    taken new changes
    Signed-off-by: Madhuravas reddy <madhu@mosip.io>

--- usecase-compass/src/main/java/io/mosip/compass/admin/mapper/UserInfoMapper.java
+++ usecase-compass/src/main/java/io/mosip/compass/admin/mapper/UserInfoMapper.java
@@ -16,6 +16,8 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Base64;
 import java.util.List;
 
@@ -24,11 +26,13 @@ public interface UserInfoMapper {
 
     Logger log = LoggerFactory.getLogger(UserInfoMapper.class);
 
+    @Mapping(target = "userInfoId", ignore = true)
     @Mapping(target = "compassId", ignore = true) // This is auto-generated
     @Mapping(target = "createdTimes", expression = "java(java.time.LocalDateTime.now())")
     @Mapping(target = "faceImageGrey", source = "faceImageColor", qualifiedByName = "convertColorBase64ToGreyBase64")
     UserInfo toEntity(UserInfoDTO dto);
 
+    @Mapping(source = "createdTimes", target = "issuanceDate", qualifiedByName = "mapCreatedTimesToIssuedDate")
     UserInfoDTO toDto(UserInfo entity);
 
     @Mapping(source = "userInfoId", target = "userInfoId")
@@ -77,6 +81,11 @@ public interface UserInfoMapper {
             log.error("Conversion of color image to grayscale image failed.", e);
             throw new RuntimeException("Failed to convert color image to grayscale Base64", e);
         }
+    }
+
+    @Named("mapCreatedTimesToIssuedDate")
+    default LocalDate mapCreatedTimesToIssuedDate(LocalDateTime createdTimes) {
+        return createdTimes != null ? createdTimes.toLocalDate() : null;
     }
 
 }
