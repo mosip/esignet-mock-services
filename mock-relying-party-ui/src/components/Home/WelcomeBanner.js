4945dce0e691e41e96ab15d689562acc882a9b52
GurukiranP <talk2gurukiran@gmail.com>
2025-04-23 12:46:00 +0530

    [MOSIP-41056] mock-rp-ui fixes for compass use case.
    Signed-off-by: GurukiranP <talk2gurukiran@gmail.com>

--- mock-relying-party-ui/src/components/Home/WelcomeBanner.js
+++ mock-relying-party-ui/src/components/Home/WelcomeBanner.js
@@ -1,56 +1,53 @@
 import React from "react";
 
-export const WelcomeBanner = (user) => {
+export const WelcomeBanner = (userInfo) => {
   return (
-      <div className="bg-white shadow rounded-3xl p-4 flex items-center justify-between w-[90%] mx-auto mt-6 mb-8">
-          <div className="flex items-center">
-              <div className="w-24 h-24 rounded-full overflow-hidden mr-4">
-                  <img
-                      alt="User Avatar"
-                      id={"user-avtar"}
-                      className="w-full h-full object-cover"
-                      src={
-                          user?.picture ? user.picture : "User-Profile-Icon.png"
-                      }
-                  />
-              </div>
-              <div>
-                  <h2 className="text-xl font-semibold text-gray-800">
-                      Welcome {user?.given_name ?? user?.name}! 👋
-                  </h2>
-                  <p className="text-sm text-gray-500">
-                      Your gateway to seamless access to trusted digital government services
-                  </p>
-              </div>
-          </div>
-
-          {/* Right Section (Search Bar) */}
-          <div className=" h-14 w-1/2 pl-8 flex border border-gray-300 rounded-md">
-              <input
-                  type="text"
-                  placeholder="Search UtopiaGov"
-                  className="focus:border-none flex-grow hover:border-none border-none  "
-              />
-              <button
-                  className=" px-8 bg-blue-500 text-white rounded-md h-14"
-              >
-                  <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5 inline-block mr-1"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                  >
-                      <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                      />
-                  </svg>
-                  Search
-              </button>
-          </div>
+    <div className="bg-white shadow rounded-3xl p-4 flex items-center justify-between w-[90%] mx-auto mt-6 mb-8">
+      <div className="flex items-center">
+        <div className="w-24 h-24 rounded-full overflow-hidden mr-4">
+          <img
+            alt="User Avatar"
+            id={"user-avtar"}
+            className="w-full h-full object-cover"
+            src={userInfo?.user?.picture ? userInfo.user.picture : ""}
+          />
+        </div>
+        <div>
+          <h2 className="text-xl font-semibold text-gray-800">
+            Welcome {userInfo?.user?.given_name ?? userInfo.user?.name}! 👋
+          </h2>
+          <p className="text-sm text-gray-500">
+            Your gateway to seamless access to trusted digital government
+            services
+          </p>
+        </div>
       </div>
+
+      {/* Right Section (Search Bar) */}
+      <div className=" h-14 w-1/2 pl-8 flex border border-gray-300 rounded-md">
+        <input
+          type="text"
+          placeholder="Search UtopiaGov"
+          className="focus:border-none flex-grow hover:border-none border-none  "
+        />
+        <button className=" px-8 bg-blue-500 text-white rounded-md h-14">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 inline-block mr-1"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+          Search
+        </button>
+      </div>
+    </div>
   );
 };
