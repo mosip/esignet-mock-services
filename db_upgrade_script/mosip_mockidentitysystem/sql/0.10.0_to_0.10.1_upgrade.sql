d353da85a8b8ac559204f6b16dd3d0ab8c437fab
bhumi46 <thisisbn46@gmail.com>
2025-05-13 13:17:39 +0530

    [ES-2178] Updated 0.10.0_to_0.10.1_upgrade.sql
    Signed-off-by: bhumi46 <thisisbn46@gmail.com>

--- db_upgrade_script/mosip_mockidentitysystem/sql/0.10.0_to_0.10.1_upgrade.sql
+++ db_upgrade_script/mosip_mockidentitysystem/sql/0.10.0_to_0.10.1_upgrade.sql
@@ -1,3 +1,5 @@
 -- Alter the `identity_json` column to be of type `VARCHAR` without a length limit
+\c mosip_mockidentitysystem
+
 ALTER TABLE mockidentitysystem.mock_identity
     ALTER COLUMN identity_json TYPE VARCHAR;
