0fe7d134a3e347e0d340029b16279d7cf00dbe19
Rakshith B <79500257+Rakshithb1@users.noreply.github.com>
2025-05-21 15:45:40 +0530

    [ES-2178] Updated 0.10.1_to_0.11.0_upgrade.sql
    Signed-off-by: Rakshith B <79500257+Rakshithb1@users.noreply.github.com>

--- db_upgrade_script/mosip_mockidentitysystem/sql/0.10.1_to_0.11.0_upgrade.sql
+++ db_upgrade_script/mosip_mockidentitysystem/sql/0.10.1_to_0.11.0_upgrade.sql
@@ -1,1 +1,5 @@
-\echo 'Upgrade Queries not required for transition from $CURRENT_VERSION to $UPGRADE_VERSION'
+-- Alter the `identity_json` column to be of type `VARCHAR` without a length limit
+\c mosip_mockidentitysystem
+
+ALTER TABLE mockidentitysystem.mock_identity
+    ALTER COLUMN identity_json TYPE VARCHAR;
