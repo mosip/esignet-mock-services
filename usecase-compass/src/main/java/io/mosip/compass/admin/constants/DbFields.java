f42f1c4a571f8b3308ac6c0acb524e3827a7671e
SubhashMurmu <146713584+SubhashMurmu@users.noreply.github.com>
2025-08-22 12:40:30 +0530

    made changes as asked (#493)
    Signed-off-by: SubhashMurmu <subhashmurmujsr2511@gmail.com>

---
+++ usecase-compass/src/main/java/io/mosip/compass/admin/constants/DbFields.java
@@ -0,0 +1,12 @@
+package io.mosip.compass.admin.constants;
+
+public final class DbFields {
+    public static final String NATIONAL_UID = "nationalUid";
+    public static final String FIRST_NAME_PRIMARY = "firstNamePrimary";
+    public static final String LAST_NAME_SECONDARY = "lastNameSecondary";
+    public static final String CARD_ACCESS_NUMBER = "cardAccessNumber";
+    public static final String CREATED_TIMES = "createdTimes";
+    public static final String PERCENT = "%";
+
+    private DbFields() {}
+}
--- usecase-compass/src/main/java/io/mosip/compass/admin/repository/UserInfoRepository.java
+++ usecase-compass/src/main/java/io/mosip/compass/admin/repository/UserInfoRepository.java
@@ -1,7 +1,11 @@
 package io.mosip.compass.admin.repository;
 
+import io.mosip.compass.admin.constants.DbFields;
 import io.mosip.compass.admin.entity.UserInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,12 +16,23 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserInfoRepository extends JpaRepository<UserInfo, UUID> {
+public interface UserInfoRepository extends JpaRepository<UserInfo, UUID>, JpaSpecificationExecutor<UserInfo> {
+
     Optional<UserInfo> findByNationalUid(String nationalUid);
 
     List<UserInfo> findAllByOrderByCreatedTimesDesc();
 
+    Page<UserInfo> findAllByOrderByCreatedTimesDesc(Pageable pageable);
+
     @Modifying
     @Query("DELETE FROM UserInfo u WHERE u.userInfoId IN :userInfoIds")
     void deleteAllByUserInfoIds(@Param("userInfoIds") List<UUID> userInfoIds);
+
+    // Example of using constants in a query method (if you switch to native queries or dynamic construction)
+    // Otherwise, you can leave '%' as a string in JPQL annotations.
+    @Query("SELECT u FROM UserInfo u WHERE LOWER(u.cardAccessNumber) LIKE LOWER(CONCAT('%', :cardAccessNumber, '%'))")
+    Page<UserInfo> findByCardAccessNumberContainingIgnoreCase(@Param("cardAccessNumber") String cardAccessNumber, Pageable pageable);
+
+    @Query("SELECT u FROM UserInfo u WHERE u.cardAccessNumber = :cardAccessNumber")
+    Optional<UserInfo> findByExactCardAccessNumber(@Param("cardAccessNumber") String cardAccessNumber);
 }
