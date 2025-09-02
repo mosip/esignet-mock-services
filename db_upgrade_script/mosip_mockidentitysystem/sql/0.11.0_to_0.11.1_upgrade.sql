9dd25c5ecd5fd4db186ba23e1794a5cc078abbf6
Md Humair Kankudti <mdhumair.kankudti@gmail.com>
2025-09-03 00:18:29 +0530

    ES mock-identity-system release to dev (#501)
    * resolve conflicts
    
    Signed-off-by: Md-Humair-KK <mdhumair.kankudti@gmail.com>
    
    * resolve conflicts
    
    Signed-off-by: Md-Humair-KK <mdhumair.kankudti@gmail.com>
    
    * docker update (#491)
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>
    
    * resolve conflicts
    
    Signed-off-by: Md-Humair-KK <mdhumair.kankudti@gmail.com>
    
    * resolve conflicts
    
    Signed-off-by: Md-Humair-KK <mdhumair.kankudti@gmail.com>
    
    * updated version to 0.11.3-SNAPSHOT
    
    Signed-off-by: Md-Humair-KK <mdhumair.kankudti@gmail.com>
    
    * POM version to 0.12.0, image tag to develop
    
    Signed-off-by: Zeeshan Mehboob <zeeshan.mehboob@infosys.com>
    
    ---------
    
    Signed-off-by: Md-Humair-KK <mdhumair.kankudti@gmail.com>
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>
    Signed-off-by: Zeeshan Mehboob <zeeshan.mehboob@infosys.com>
    Co-authored-by: ase-101 <sunkadaeanusha@gmail.com>
    Co-authored-by: Sachin Rana <sacrana324@gmail.com>
    Co-authored-by: Zeeshan Mehboob <82993262+zesu22@users.noreply.github.com>
    Co-authored-by: Zeeshan Mehboob <zeeshan.mehboob@infosys.com>

--- db_upgrade_script/mosip_mockidentitysystem/sql/0.11.0_to_0.11.1_upgrade.sql
+++ db_upgrade_script/mosip_mockidentitysystem/sql/0.11.0_to_0.11.1_upgrade.sql
@@ -1,10 +1,1 @@
-\c mosip_mockidentitysystem
-
-CREATE TABLE mockidentitysystem.rp_data (
-    rp_id character varying(100) NOT NULL,
-    client_id character varying(255),
-    encryption_key text,
-    status character varying(50),
-    cr_dtimes timestamp NOT NULL,
-    CONSTRAINT pk_rp_data_rp_id PRIMARY KEY (rp_id)
-);
+\echo 'Upgrade Queries not required for transition from $CURRENT_VERSION to $UPGRADE_VERSION'
