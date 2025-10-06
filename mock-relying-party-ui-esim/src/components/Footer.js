8b350dd15d41eaf6a7ed05a7ad2e71b33a681a9d
GurukiranP <talk2gurukiran@gmail.com>
2025-10-06 14:52:54 +0530

    [ES-2192] Fyntel app UI fixes.
    Signed-off-by: GurukiranP <talk2gurukiran@gmail.com>

--- mock-relying-party-ui-esim/src/components/Footer.js
+++ mock-relying-party-ui-esim/src/components/Footer.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 function Footer() {
   const { t } = useTranslation();
 
   return (
-    <footer className="text-center py-5 text-gray-600 text-sm bg-white mt-10">
-      {t('footer.copyright')}
+    <footer className="text-center py-5 text-gray-600 text-sm bg-white">
+      {t("footer.copyright")}
     </footer>
   );
 }
