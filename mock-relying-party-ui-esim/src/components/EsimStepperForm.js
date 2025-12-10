6158567d557d74f52ba03093a17af71147b9284a
Zeeshan Mehboob <zeeshan.mehboob@infosys.com>
2025-12-10 09:34:22 +0530

    [ES-2192] esim UI fixes (#497)
    Signed-off-by: Zeeshan Mehboob <zeeshan.mehboob@infosys.com>

--- mock-relying-party-ui-esim/src/components/EsimStepperForm.js
+++ mock-relying-party-ui-esim/src/components/EsimStepperForm.js
@@ -16,23 +16,12 @@ const EsimStepperForm = ({ userInfo, address, onSubmitSuccess }) => {
   const isFormComplete = isPlanSelected && isDeviceValid;
 
   const info = [];
-
   if (userInfo) {
-    if (userInfo.name) {
-      info.push({ label: t('personalInfo.fullName'), value: userInfo.name });
-    }
-    if (userInfo.phone_number) {
-      info.push({ label: t('personalInfo.mobileNumber'), value: userInfo.phone_number });
-    }
-    if (userInfo.email) {
-      info.push({ label: t('personalInfo.email'), value: userInfo.email });
-    }
-    if (userInfo.gender) {
-      info.push({ label: t('personalInfo.gender'), value: userInfo.gender });
-    }
-    if (userInfo.birthdate) {
-      info.push({ label: t('personalInfo.dateOfBirth'), value: userInfo.birthdate });
-    }
+    info.push({ label: t('personalInfo.fullName'), value: userInfo.name || null });
+    info.push({ label: t('personalInfo.mobileNumber'), value: userInfo.phone_number || null });
+    info.push({ label: t('personalInfo.email'), value: userInfo.email || null });
+    info.push({ label: t('personalInfo.gender'), value: userInfo.gender || null });
+    info.push({ label: t('personalInfo.dateOfBirth'), value: userInfo.birthdate || null });
     if (address) {
       info.push({ label: t('personalInfo.address'), value: address });
     }
@@ -80,15 +69,15 @@ const EsimStepperForm = ({ userInfo, address, onSubmitSuccess }) => {
                   <img src={`/images/step-${step}.svg`} alt={`Step ${step}`} className="w-4 h-4" />
                 </div>
                 <div className={`pl-[9px] font-semibold text-gray-700`}>
-                  <div className="font-semibold w-[137px] h-4 leading-4">{t('stepper.stepLabel', { step: step })}</div>
-                  <div className="text-sm font-normal mt-1 leading-6">
+                  <div className="font-bold text-sm leading-4 uppercase tracking-[0.16px] text-[#696B6F]">{t('stepper.stepLabel', { step: step })}</div>
+                  <div className="mt-1 font-bold text-[15.5px] leading-6 tracking-[-0.16px] text-[#292C31]">
                     {step === 1 ? t('stepper.step1.description') : step === 2 ? t('stepper.step2.description') : t('stepper.step3.description')}
                   </div>
                 </div>
               </div>
               <div className="flex items-center">
                 <img
-                  src="/Images/drop-menu.svg"
+                  src="/images/drop-menu.svg"
                   alt="Toggle"
                   className={`w-[30px] h-[30px] rounded-full bg-[#E8F7FF] border-[0.63px] border-[#292C31] transition-transform duration-200 ${expandedStep === step ? 'rotate-180' : ''}`}
                   style={{ borderWidth: '0.63px' }}
@@ -126,7 +115,7 @@ const EsimStepperForm = ({ userInfo, address, onSubmitSuccess }) => {
                       ))}
                     </div>
                     <div className="bg-gray-100 rounded-[12px] p-4 flex items-start gap-3 border-[1px] border-[#E2E2E4] w-full lg:hidden mx-auto">
-                      <img src="/Images/info-circle-icon.svg" alt="Info Icon" className="w-5 h-5 mt-0.5" />
+                      <img src="/images/info-circle-icon.svg" alt="Info Icon" className="w-5 h-5 mt-0.5" />
                       <p className="text-sm text-gray-700 leading-snug">
                         <strong className="block mb-1">{t('plans.info.title')}</strong>
                         <Trans i18nKey="plans.info.content">
@@ -182,7 +171,7 @@ const EsimStepperForm = ({ userInfo, address, onSubmitSuccess }) => {
                         </div>
 
                         <div className="col-span-2 bg-gray-100 rounded-[12px] p-4 flex items-start gap-3 border-[1px] border-[#E2E2E4] h-[155px]">
-                          <img src="/Images/info-circle-icon.svg" alt="Info Icon" className="w-5 h-5 mt-0.5 flex-shrink-0" />
+                          <img src="/images/info-circle-icon.svg" alt="Info Icon" className="w-5 h-5 mt-0.5 flex-shrink-0" />
                           <p className="text-sm text-gray-700 leading-snug">
                             <strong className="block mb-1">{t('plans.info.title')}</strong>
                             <Trans i18nKey="plans.info.content">
@@ -217,7 +206,7 @@ const EsimStepperForm = ({ userInfo, address, onSubmitSuccess }) => {
                             {t('device.eid')}<span className="text-red-500">*</span>
                           </label>
                           <div className="relative group flex items-center">
-                            <img src="/Images/info-circle-icon.svg" alt="Info for EID" className="w-4 h-4 ml-1.5 cursor-pointer" />
+                            <img src="/images/info-circle-icon.svg" alt="Info for EID" className="w-4 h-4 ml-1.5 cursor-pointer" />
                             <div className="absolute left-full top-1/2 -translate-y-1/2 ml-2 w-max max-w-xs
                                             invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity duration-300
                                             bg-gray-100 text-gray-800 rounded-lg p-3 shadow-lg z-10 border border-gray-200">
@@ -252,7 +241,7 @@ const EsimStepperForm = ({ userInfo, address, onSubmitSuccess }) => {
                             {t('device.imei')}<span className="text-red-500">*</span>
                           </label>
                           <div className="relative group flex items-center">
-                            <img src="/Images/info-circle-icon.svg" alt="Info for IMEI" className="w-4 h-4 ml-1.5 cursor-pointer" />
+                            <img src="/images/info-circle-icon.svg" alt="Info for IMEI" className="w-4 h-4 ml-1.5 cursor-pointer" />
                             <div className="absolute left-full top-1/2 -translate-y-1/2 ml-2 w-max max-w-xs
                                             invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity duration-300
                                             bg-gray-100 text-gray-800 rounded-lg p-3 shadow-lg z-10 border border-gray-200">
@@ -306,95 +295,50 @@ const EsimStepperForm = ({ userInfo, address, onSubmitSuccess }) => {
                           <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                           <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                         </svg>
-                        <p className="text-lg font-medium">
-                          {t('loading.userDetails', 'Loading User Details...')}
-                        </p>
+                        <p className="text-lg font-medium">{t('loading.userDetails', 'Loading User Details...')}</p>
                       </div>
                     ) : (
-                      // Grid layout with all responsive Verified Tags
                       <>
                         <div className="grid grid-cols-1 sm:grid-cols-2 sm:gap-x-8 gap-y-4 my-4">
-                          {/* Render the first info field */}
-                          {info.length > 0 && (
-                            <div className="col-span-1">
-                              {/* Mobile View */}
-                              <div className="lg:hidden">
-                                <div className="flex items-center mb-1">
-                                  <span className="text-sm font-medium text-gray-700">{info[0].label}</span>
-                                  <div className="flex items-center ml-2 text-[#4CAF50]">
-                                    <img src="/Images/tick.svg" alt="Verified" className="w-4 h-4" />
-                                    <span className="ml-1 text-xs font-medium">{t('personalInfo.verified', 'Verified')}</span>
-                                  </div>
-                                </div>
-                                <input type="text" value={info[0].value} disabled className="p-3 w-full rounded-md text-gray-600 bg-gray-100 border-gray-300 cursor-not-allowed" />
-                              </div>
-                              {/* Desktop View */}
-                              <div className="hidden lg:block">
-                                <span className="text-sm font-medium text-gray-700">{info[0].label}</span>
-                                <div className="flex items-center gap-4 mt-1">
-                                  <input type="text" value={info[0].value} disabled className="p-3 w-full rounded-md text-gray-600 bg-gray-100 border-gray-300 cursor-not-allowed" />
-                                  <div className="flex-shrink-0 flex items-center justify-center bg-[#F2FFF9] border border-[#C6EDDB] rounded-md h-[44px] px-3 text-[#4CAF50] text-sm font-medium">
-                                    <img src="/Images/tick.svg" alt="Tick" className="w-5 h-5" />
-                                    <span className="ml-2">{t('personalInfo.verified', 'Verified')}</span>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          )}
 
-                          {/* Render the Photo with its responsive Verified Tag */}
+                          {/* Renders the Photo */}
                           {userInfo.picture && (
-                            <div className="col-span-1 sm:row-span-3 order-first sm:order-none">
-                              {/* Mobile View for Photo */}
-                              <div className="lg:hidden">
-                                <div className="flex items-center mb-1">
-                                  <span className="text-sm font-medium text-gray-700">{t('personalInfo.photoLabel', 'Photo')}</span>
-                                  <div className="flex items-center ml-2 text-[#4CAF50]">
-                                    <img src="/Images/tick.svg" alt="Verified" className="w-4 h-4" />
-                                    <span className="ml-1 text-xs font-medium">{t('personalInfo.verified', 'Verified')}</span>
-                                  </div>
-                                </div>
-                                <img src={userInfo.picture} alt={t('personalInfo.userPhotoAlt', 'User Photo')} className="w-32 h-32 rounded-md object-cover border-2 border-gray-200 shadow-md" />
-                              </div>
-                              {/* Desktop View for Photo */}
-                              <div className="hidden lg:block">
-                                <span className="text-sm font-medium text-gray-700">{t('personalInfo.photoLabel', 'Photo')}</span>
-                                <div className="flex items-end gap-4 mt-1">
-                                  <img src={userInfo.picture} alt={t('personalInfo.userPhotoAlt', 'User Photo')} className="w-32 h-32 rounded-md object-cover border-2 border-gray-200 shadow-md" />
-                                  <div className="flex-shrink-0 flex items-center justify-center bg-[#F2FFF9] border border-[#C6EDDB] rounded-md h-[44px] px-3 text-[#4CAF50] text-sm font-medium">
-                                    <img src="/Images/tick.svg" alt="Tick" className="w-5 h-5" />
-                                    <span className="ml-2">{t('personalInfo.verified', 'Verified')}</span>
+                            <div className="col-span-1 sm:col-start-2 sm:row-start-1 sm:row-span-2">
+                              <div className="flex items-center gap-2">
+                                <label className="text-xs sm:text-sm font-medium text-gray-700 block">{t('personalInfo.photoLabel', 'Photo')}</label>
+                                <div className="relative group">
+                                  <img src="/images/tick.svg" alt="Verified Icon" className="w-4 h-4" />
+                                  <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 w-max invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity bg-gray-800 text-white text-xs font-medium rounded py-1 px-2 z-10">
+                                    {t('personalInfo.verified', 'Verified')}
+                                    <svg className="absolute text-gray-800 h-2 w-full left-0 top-full" x="0px" y="0px" viewBox="0 0 255 255"><polygon className="fill-current" points="0,0 127.5,127.5 255,0" /></svg>
                                   </div>
                                 </div>
                               </div>
+                              <img src={userInfo.picture} alt={t('personalInfo.userPhotoAlt', 'User Photo')} className="w-32 h-32 rounded-md object-cover border-2 border-gray-200 shadow-md mt-1" />
                             </div>
                           )}
 
-                          {/* Render the rest of the info fields */}
-                          {info.slice(1).map((field) => (
+                          {/* Renders all text-based fields */}
+                          {info.map((field) => (
                             <div className="col-span-1" key={field.label}>
-                              {/* Mobile View */}
-                              <div className="lg:hidden">
-                                <div className="flex items-center mb-1">
-                                  <span className="text-sm font-medium text-gray-700">{field.label}</span>
-                                  <div className="flex items-center ml-2 text-[#4CAF50]">
-                                    <img src="/Images/tick.svg" alt="Verified" className="w-4 h-4" />
-                                    <span className="ml-1 text-xs font-medium">{t('personalInfo.verified', 'Verified')}</span>
+                              <div className="flex items-center gap-2">
+                                <label className="text-xs sm:text-sm font-medium text-gray-700 block">{field.label}</label>
+                                {field.value && (
+                                  <div className="relative group">
+                                    <img src="/images/tick.svg" alt="Verified Icon" className="w-4 h-4" />
+                                    <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 w-max invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity bg-gray-800 text-white text-xs font-medium rounded py-1 px-2 z-10">
+                                      {t('personalInfo.verified', 'Verified')}
+                                      <svg className="absolute text-gray-800 h-2 w-full left-0 top-full" x="0px" y="0px" viewBox="0 0 255 255"><polygon className="fill-current" points="0,0 127.5,127.5 255,0" /></svg>
+                                    </div>
                                   </div>
-                                </div>
-                                <input type="text" value={field.value} disabled className="p-3 w-full rounded-md text-gray-600 bg-gray-100 border-gray-300 cursor-not-allowed" />
+                                )}
                               </div>
-                              {/* Desktop View */}
-                              <div className="hidden lg:block">
-                                <span className="text-sm font-medium text-gray-700">{field.label}</span>
-                                <div className="flex items-center gap-4 mt-1">
-                                  <input type="text" value={field.value} disabled className="p-3 w-full rounded-md text-gray-600 bg-gray-100 border-gray-300 cursor-not-allowed" />
-                                  <div className="flex-shrink-0 flex items-center justify-center bg-[#F2FFF9] border border-[#C6EDDB] rounded-md h-[44px] px-3 text-[#4CAF50] text-sm font-medium">
-                                    <img src="/Images/tick.svg" alt="Tick" className="w-5 h-5" />
-                                    <span className="ml-2">{t('personalInfo.verified', 'Verified')}</span>
-                                  </div>
-                                </div>
-                              </div>
+                              <input
+                                type="text"
+                                value={field.value || ''}
+                                disabled
+                                className="p-3 w-full rounded-md text-gray-600 bg-gray-100 border-gray-300 cursor-not-allowed mt-1"
+                              />
                             </div>
                           ))}
                         </div>
@@ -402,10 +346,7 @@ const EsimStepperForm = ({ userInfo, address, onSubmitSuccess }) => {
                         {/* Submit button */}
                         <div className="flex justify-end mt-6">
                           <button
-                            className={`w-full h-[48px] lg:w-[381px] border-[1px] rounded-[8px] font-medium ${!isFormComplete
-                              ? 'bg-gray-300 text-gray-500 cursor-not-allowed'
-                              : 'bg-blue-600 border-[#017DC0] text-white hover:bg-blue-700'
-                              }`}
+                            className={`w-full h-[48px] lg:w-[381px] border-[1px] rounded-[8px] font-medium ${!isFormComplete ? 'bg-gray-300 text-gray-500 cursor-not-allowed' : 'bg-blue-600 border-[#017DC0] text-white hover:bg-blue-700'}`}
                             onClick={handleSubmit}
                             disabled={!isFormComplete}
                           >
