53d09210020ee1a43e2ce15217a09b845b127101
Vinay <vinayadattakavuluri@gmail.com>
2026-02-02 12:10:47 +0530

    feat(MOSIP-41056): update branding and assets for usecase-compass-ui
    Signed-off-by: Vinay <vinayadattakavuluri@gmail.com>

--- usecase-compass-ui/src/components/LandingPage.jsx
+++ usecase-compass-ui/src/components/LandingPage.jsx
@@ -10,9 +10,9 @@ function LandingPage() {
       <div className="bg-[url('/assets/world_polygon_map.png')] bg-[length:100%_100%] bg-center bg-no-repeat h-full w-full flex items-center justify-center relative">
         <div className="w-[616px] h-[455px] bg-[#ffffff] rounded-2xl shadow flex items-center text-center justify-center max-[1200px]:w-[70%] max-[800px]:w-[90%]">
           <div>
-            <h1 className="text-[#020548] text-[82px] font-bold mb-0">comPASS</h1>
+            <h1 className="text-[#020548] text-[82px] font-bold mb-0">GlobalID Pass</h1>
             <h1 className="text-[#020548] text-[62px] font-bold -mt-7">Admin Portal</h1>
-            <p className="text-[#0033A0] text-xl font-bold">comPass Credential Issuance</p>
+            <p className="text-[#0033A0] text-xl font-bold">GlobalID Pass Credential Issuance</p>
             <button onClick={() => keycloak.login({ redirectUri: window.location.origin + '/home' })} className="h-[44px] w-[360px] bg-[#FF671F] rounded-lg text-[#ffffff] mt-4 cursor-pointer">Sign In</button>
           </div>
         </div>
