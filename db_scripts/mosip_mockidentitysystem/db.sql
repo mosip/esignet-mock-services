f7860fb7ca970ae31f521fc1792b9b302a43960e
Abhi <abhishek.shankarcs@gmail.com>
2026-05-20 15:44:11 +0530

    [MOSIP-37808] Updated DB attributes of MOSIP esignet-mock
    Signed-off-by: Abhi <abhishek.shankarcs@gmail.com>

--- db_scripts/mosip_mockidentitysystem/db.sql
+++ db_scripts/mosip_mockidentitysystem/db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE mosip_mockidentitysystem
+CREATE DATABASE :mosipdbname
 	ENCODING = 'UTF8' 
 	LC_COLLATE = 'en_US.UTF-8' 
 	LC_CTYPE = 'en_US.UTF-8' 
@@ -6,12 +6,12 @@ CREATE DATABASE mosip_mockidentitysystem
 	OWNER = postgres
 	TEMPLATE  = template0;
 
-COMMENT ON DATABASE mosip_mockidentitysystem IS 'Mock identity related data is stored in this database';
+COMMENT ON DATABASE :mosipdbname IS 'Mock identity related data is stored in this database';
 
 \c mosip_mockidentitysystem postgres
 
 DROP SCHEMA IF EXISTS mockidentitysystem CASCADE;
 CREATE SCHEMA mockidentitysystem;
 ALTER SCHEMA mockidentitysystem OWNER TO postgres;
-ALTER DATABASE mosip_mockidentitysystem SET search_path TO mockidentitysystem,pg_catalog,public;
+ALTER DATABASE :mosipdbname SET search_path TO mockidentitysystem,pg_catalog,public;
 
